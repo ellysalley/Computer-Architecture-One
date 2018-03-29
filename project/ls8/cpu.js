04fc53323844563a2514c3903369e29fb15544f4
Elly <ellyhahn@gmail.com>
2018-03-28 20:53:14 -0700

    [CS6] Elly Han - done so far

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -2,6 +2,13 @@
  * LS-8 v2.0 emulator skeleton code
  */
 
+const HLT = 0b00000001;
+const LDI = 0b10011001;
+const PRN = 0b01000011;
+const MUL = 0b10101010;
+const PUSH = 0b01001101;
+const POP = 0b01001100;
+
 /**
  * Class for simulating a simple Computer (CPU & memory)
  */
@@ -57,7 +64,7 @@ class CPU {
     alu(op, regA, regB) {
         switch (op) {
             case 'MUL':
-                // !!! IMPLEMENT ME
+                this.reg[regA] = this.reg[regA] * this.reg[regB];
                 break;
         }
     }
@@ -69,8 +76,7 @@ class CPU {
         // Load the instruction register (IR--can just be a local variable here)
         // from the memory address pointed to by the PC. (I.e. the PC holds the
         // index into memory of the next instruction.)
-
-        // !!! IMPLEMENT ME
+        let IR = this.ram.read(this.reg.PC);
 
         // Debugging output
         //console.log(`${this.reg.PC}: ${IR.toString(2)}`);
@@ -78,19 +84,87 @@ class CPU {
         // Get the two bytes in memory _after_ the PC in case the instruction
         // needs them.
 
-        // !!! IMPLEMENT ME
+        let operandA = this.ram.read(this.reg.PC + 1);
+        let operandB = this.ram.read(this.reg.PC + 2);
 
         // Execute the instruction. Perform the actions for the instruction as
         // outlined in the LS-8 spec.
 
-        // !!! IMPLEMENT ME
+        /*
+        switch (IR) {
+
+            case HLT:
+                this.stopClock();
+                break;
+
+            case LDI:
+                this.reg[operandA] = operandB;
+                break;
+
+            case PRN:
+                console.log(this.reg[operandA]);
+                break;
+
+            case MUL:
+                this.alu('MUL', operandA, operandB);
+                break;
+
+            default:
+                console.log("Unknown instruction: " + IR.toString(2));
+                this.stopClock();
+                break;
+        }
+        */
+        const handleHLT = () => {
+            this.stopClock();
+        };
+
+        const handleLDI = (operandA, operandB) => {
+            this.reg[operandA] = operandB;
+        };
+
+        const handlePRN = (operandA) => {
+            console.log(this.reg[operandA]);
+        };
+
+        const handleMUL = (operandA, operandB) => {
+            this.reg[operandA] = this.alu('MUL', operandA, operandB);
+        };
+
+        const handlePush = () => {
+            this.reg[7] -= 1;
+            this.ram.write(this.reg[7], this.reg[operandA]);
+        };
+
+        const handlePop = () => {
+            this.reg[operandA] = this.ram.read(this.reg[7]);
+            this.reg[7] += 1;
+        };
+
+        const handleDefault = (instruction) => {
+            console.log("Unknown instruction: " + IR.toString(2));
+            handleHLT();
+        };
+
+        const branchTable = {
+            [LDI]: handleLDI,
+            [PRN]: handlePRN, 
+            [MUL]: handleMUL,
+            [HLT]: handleHLT,
+        };
+
+        if (Object.keys(branchTable).includes(IR.toString())) {
+            branchTable[IR](operandA, operandB);
+        } else {
+            handleDefault(IR);
+        };
 
         // Increment the PC register to go to the next instruction. Instructions
         // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
         // instruction byte tells you how many bytes follow the instruction byte
         // for any particular instruction.
         
-        // !!! IMPLEMENT ME
+        this.reg.PC += (IR >>> 6) + 1;
     }
 }
 
--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -1,39 +1,91 @@
+const readline = require('readline');
+const fs = require('fs');
 const RAM = require('./ram');
 const CPU = require('./cpu');
+
+const ram = new RAM(256);
+const cpu = new CPU(ram);
 
 /**
  * Load an LS8 program into memory
  *
  * TODO: load this from a file on disk instead of having it hardcoded
  */
-function loadMemory() {
+function loadMemory(cpu) {
 
     // Hardcoded program to print the number 8 on the console
 
-    const program = [ // print8.ls8
-        "10011001", // LDI R0,8  Store 8 into R0
-        "00000000",
-        "00001000",
-        "01000011", // PRN R0    Print the value in R0
-        "00000000",
-        "00000001"  // HLT       Halt and quit
-    ];
+    // const program = [ // print8.ls8
+    //     "10011001", // LDI R0,8  Store 8 into R0
+    //     "00000000",
+    //     "00001000",
+    //     "01000011", // PRN R0    Print the value in R0
+    //     "00000000",
+    //     "00000001"  // HLT       Halt and quit
+    // ];
+
+    // const program = [
+    //     "10011001",
+    //     "00000000",
+    //     "00001000",
+    //     "10011001",
+    //     "00000001",
+    //     "00001001",
+    //     "10101010",
+    //     "00000000",
+    //     "00000001",
+    //     "01000011",
+    //     "00000000",
+    //     "00000001"
+    // ];
+
 
     // Load the program into the CPU's memory a byte at a time
+    /*
     for (let i = 0; i < program.length; i++) {
         cpu.poke(i, parseInt(program[i], 2));
     }
+    */
+
+    let program = [];
+    if (process.argv.length === 3) {
+        var lineReader = readline.createInterface({
+            input: fs.createReadStream(process.argv[2])
+        });
+        lineReader.on('line', function(line) {
+            // console.log('Line from file:', line);
+    
+            // let str = line.split('#')[0].slice(0, 8);
+            let byteAndComment = line.split('#');
+            let byteStr = byteAndComment[0];
+            // bytetr = byteStr.slice(0, 8);
+            byteStr = byteStr.trim();
+    
+            // let byte = parseInt(byteStr, 2);
+    
+            // if (!isNaN(byte)) {
+            //     cpu.poke(address++, byte);
+            // }
+            if (byteStr > 1) {
+                program.push(byteStr);
+            } else {
+                return;
+            }
+        });
+        lineReader.on('close', function() {
+            processProgram(program, cpu);
+        });
+    } else {
+        console.log('provide a .ls8 file');
+    }
+};
+
+function processProgram(arr, cpu) {
+    for (let i = 0; i < arr.length; i++) {
+        cpu.poke(i, parseInt(arr[i], 2));
+    }
+    cpu.startClock();
 }
 
-/**
- * Main
- */
-
-let ram = new RAM(256);
-let cpu = new CPU(ram);
-
-// TODO: get name of ls8 file to load from command line
 
 loadMemory(cpu);
-
-cpu.startClock();
